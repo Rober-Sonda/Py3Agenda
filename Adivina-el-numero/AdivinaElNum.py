340c8ab6254d4b7ac34a4e7de19ca9ceb8071a2b
robersonda <rober.junin@gmail.com>
2020-12-13 22:30:41 +0000

    Ajuste de validaciones

--- Adivina-el-numero/AdivinaElNum.py
+++ Adivina-el-numero/AdivinaElNum.py
@@ -10,26 +10,29 @@ def jugarAdivina():
   print("Bienvenido a este juego, adivina un número del {} al {}".format(numero_inicio,numero_final))
   
   while vidas > 0:
-    numerointento=int(input("Ingresá un número-> "))
-    if numerointento == ElNumero:
-      print("Acertaste!! Felicitaciones Crack!")
-      print("Te quedaste sin vidas!!")
-      print("Gracias por jugar a \"Adivina un número\"")
-      JugarOtraVez = input("¿Querés jugar otra vez?: si | no -> )
-      if JugarOtraVez.lower() =="si":
-        jugarAdivina()
-      else:
-        print("Gracias por jugar a \"Adivina un número\"")
-        break
+    try:
+      numerointento=int(input("Ingresá un número-> "))
+    except ValueError:
+      print("Esa opción no es correcta")
     else:
-      if numerointento > ElNumero:
-        print("Fallaste!, El nùmero es menor")
-        vidas-=1
-        print("Te quedan {} vidas".format(vidas))
+      if numerointento == ElNumero:
+        print("Acertaste!! Felicitaciones Crack!")
+        print("Gracias por jugar a \"Adivina un número\"")
+        JugarOtraVez = input("¿Querés jugar otra vez?: si | no -> ")
+        if JugarOtraVez.lower() =="si":
+          jugarAdivina()
+        else:
+          print("Gracias por jugar a \"Adivina un número\"")
+          break
       else:
-        print("Fallaste!, El nùmero es mayor")
-        vidas-=1
-        print("Te quedan {} vidas".format(vidas))
+        if numerointento > ElNumero:
+          print("Fallaste!, El nùmero es menor")
+          vidas-=1
+          print("Te quedan {} vidas".format(vidas))
+        else:
+          print("Fallaste!, El nùmero es mayor")
+          vidas-=1
+          print("Te quedan {} vidas".format(vidas))
   else:
     print("Te quedaste sin vidas!!")
     print("El número era el {} ".format(ElNumero))
